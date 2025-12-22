049fb7e24d2a1a49b074df43a74c21de00697d7e
alsuhaibi96 <alsuhaibiabdulrahman@gmail.com>
2025-12-22 14:02:50 +0300

    init: project init

--- src/test/java/org/example/sprintboot/SprintBootApplicationTests.java
+++ src/test/java/org/example/sprintboot/SprintBootApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package org.example.sprintboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SprintBootApplicationTests {
 
     @Test
     void contextLoads() {
